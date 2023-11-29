a531d6cbdf7f2bb64d8c06f6549c4e2f3f49fd78
Maxim Alyoshin <maxim.alyosh@gmail.com>
2023-11-29 15:45:28 +0500

    feat: add func tabs switch and tea-cup, dessert

--- coffee-house/scripts/menu.js
+++ coffee-house/scripts/menu.js
@@ -1,5 +1,14 @@
-let currentUrl = window.location.href;
 const coffeeMenuLink = document.querySelector('.coffee-menu__link');
+const tabCoffeeBtn = document.querySelector('.tab-coffee');
+const tabTeaBtn = document.querySelector('.tab-tea');
+const tabDessertBtn = document.querySelector('.tab-dessert');
+let tabInd = 0;
+const tabsBtn = [tabCoffeeBtn, tabTeaBtn, tabDessertBtn];
+const cupsCards = Array.from(document.querySelector('.menu__container').children);
+
+tabCoffeeBtn.addEventListener('click', () => switchTabs(0));
+tabTeaBtn.addEventListener('click', () => switchTabs(1));
+tabDessertBtn.addEventListener('click', () => switchTabs(2));
 
 function showUnderline() {
     let currentUrl = window.location.href;
@@ -8,4 +17,18 @@ function showUnderline() {
     }
 };
 
+function switchTabs(cupInd = 0) {
+    tabInd = cupInd;
+    cupsCards.forEach((item, ind) => {
+        if (ind === tabInd) {
+            item.style.display = 'flex';
+            tabsBtn[ind].classList.add('active');
+        } else {
+            item.style.display = 'none';
+            tabsBtn[ind].classList.remove('active');
+        }
+    })
+}
+
 showUnderline()
+switchTabs()
